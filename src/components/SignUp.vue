41f9cbe07dc654e61b5e3743680500827c6e5e97
Md Asifuzzaman Suvo <asifshuvo119181@gmail.com>
2022-01-17 17:26:32 +0600

    first commit

---
+++ src/components/SignUp.vue
@@ -0,0 +1,17 @@
+<template lang="">
+    <div>
+        <h1>Sign up</h1>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SignUp",
+    
+}
+</script>
+
+<style lang="">
+    
+</style>
