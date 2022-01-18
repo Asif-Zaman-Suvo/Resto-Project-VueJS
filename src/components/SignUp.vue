7440cc303cdf720b6099e0b943b4f7dd9a66dccd
Md Asifuzzaman Suvo <asifshuvo119181@gmail.com>
2022-01-18 20:51:10 +0600

    json server installed

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,23 +1,43 @@
 <template lang="">
-    <div>
-         <img class="logo" src="../assets/logo.jpg"/>
-        <h1>Sign up</h1>
-        
-    </div>
+  <div>
+    <img class="logo" src="../assets/logo.jpg" />
+    <h1>Sign up</h1>
+  </div>
 
-    <div class="register">
-        <input type="text" class="" placeholder="Name"/>
-        <input type="email" class="" placeholder="Email"/>
-        <input type="password" class="" placeholder="Password"/>
-        <button>Sign Up</button>
-
-    </div>
-   
+  <div class="register">
+    <input type="text" v-model="name" class="" placeholder="Name" />
+    <input type="email" v-model="email" class="" placeholder="Email" />
+    <input type="password" v-model="password" class="" placeholder="Password" />
+    <button v-on:click="signUp">Sign Up</button>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "SignUp",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async signUp() {
+      let result = await axios.post("http://localhost:3000/users", {
+        email: this.email,
+        name: this.name,
+        password: this.password,
+      });
+
+      console.log(result);
+      if (result.status == 201) {
+        alert("sign up done");
+        localStorage.setItem("User-Info", JSON.stringify(result.data))
+      }
+    },
+  },
 };
 </script>
 
@@ -34,7 +54,7 @@ export default {
   margin-bottom: 30px;
   margin-right: auto;
   margin-left: auto;
-  border:1px solid skyblue;
+  border: 1px solid skyblue;
 }
 
 .register button {
@@ -43,6 +63,6 @@ export default {
   background: skyblue;
   color: white;
   cursor: pointer;
-  border:1px solid skyblue;
+  border: 1px solid skyblue;
 }
 </style>
